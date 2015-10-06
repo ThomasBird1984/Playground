8fb6a0335af750fc1a455a5859ab150ccbc20169
Thomas Bird <tbird@printsites.com>
2015-10-05 23:21:36 -0400

    Added a php file that shows how to call various functions in php

---
+++ php/callbacks.php
@@ -0,0 +1,31 @@
+<?php
+
+class test {
+
+	public function add( $num1, $num2, $callback ) {
+		$sum = $num1 + $num2;
+
+		$callback( $sum );
+	}
+
+	public function call_from_string( $num1, $num2, $callbackString ) {
+
+		$sum = $num1 + $num2;
+
+		call_user_func( $callbackString, array( $num1, $num2, $sum ) );
+	}
+}
+
+function display_string( $params ) {
+	foreach( $params as $key => $param ) {
+		echo '<p>Params: '. $key .' = '. $param;
+	}
+}
+
+$test = new test;
+
+$test->add( 1, 1, function( $result ) {
+	echo $result;
+});
+
+$test->call_from_string( 2, 3, 'display_string');
